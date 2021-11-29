28ee47b9dd14f30e0117d5f968f3f14e42567ee2
Albert Kamalov <akamalov@sbdagroup.com>
2021-11-29 09:29:03 +0300

    Homework 16-io

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,34 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private String fileName;
 
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+        ObjectMapper mapper = new ObjectMapper();
+
+        var file = new File(fileName);
+        try {
+            mapper.writeValue(file, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Data saved to the file:" + file.getAbsolutePath());
+
+
     }
 }
--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -4,12 +4,18 @@ import ru.otus.model.Measurement;
 
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        //группирует выходящий список по name, при этом суммирует поля value
-        return null;
+        Map<String, Double> result = new TreeMap<>();
+
+        data.forEach(measurement ->
+                result.merge(measurement.getName(), measurement.getValue(), Double::sum)
+        );
+
+        return result;
     }
 }
